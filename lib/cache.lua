c84b7ffdd2cf7db215d24a47786b98da50126d48
Billiam <billiamthesecond@gmail.com>
2024-12-23 23:48:02 -0600

    Add cache module

---
+++ lib/cache.lua
@@ -0,0 +1,26 @@
+local Api = require("lib/hardcover_api")
+local User = require("lib/user")
+
+local Cache = {}
+Cache.__index = Cache
+
+function Cache:new(o)
+  return setmetatable(o, self)
+end
+
+function Cache:updateBookStatus(filename, status, privacy_setting_id)
+  local settings = self.settings:readBookSettings(filename)
+  local book_id = settings.book_id
+  local edition_id = settings.edition_id
+
+  self.state.book_status = Api:updateUserBook(book_id, status, privacy_setting_id, edition_id) or {}
+end
+
+function Cache:cacheUserBook()
+  local status, errors = Api:findUserBook(self.settings:getLinkedBookId(), User:getId())
+  self.state.book_status = status or {}
+
+  return errors
+end
+
+return Cache
