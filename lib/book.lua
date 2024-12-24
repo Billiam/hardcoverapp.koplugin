32e20de7dd8616ff55b26fa498e7c7d2bd84d8f9
Billiam <billiamthesecond@gmail.com>
2024-12-23 18:20:42 -0600

    Add book model

---
+++ lib/book.lua
@@ -0,0 +1,42 @@
+local Book = {}
+
+function Book:parseIdentifiers(identifiers)
+  local result = {}
+
+  if not identifiers then
+    return result
+  end
+
+  -- TODO: are multiple identifiers comma/semicolon delimited?
+  for line in identifiers:gmatch("%s*([^%s]+)%s*") do
+    -- check for hardcover: and hardcover-edition:
+    local hc = string.match(line, "hardcover:([%w_-]+)")
+    if hc then
+      result.book_slug = hc
+    end
+
+    local hc_edition = string.match(line, "hardcover%-edition:(%d+)")
+
+    if hc_edition then
+      result.edition_id = hc_edition
+    end
+
+    if not hc and not hc_edition then
+      -- strip prefix
+      local str = string.gsub(line, "^[^%s]+%s*:%s*", "")
+
+      if str then
+        local len = #str
+
+        if len == 13 then
+          result.isbn_13 = str
+        elseif len == 10 then
+          result.isbn_10 = str
+        end
+      end
+    end
+  end
+  return result
+end
+
+return Book
