fc88c00ad19a0dcd5bb00fd72e49d9588b7f7605
Billiam <billiamthesecond@gmail.com>
2024-12-24 16:50:20 -0600

    Add hardcover and github modules for data fetching

---
+++ lib/github.lua
@@ -0,0 +1,37 @@
+local https = require("ssl.https")
+local json = require("json")
+local ltn12 = require("ltn12")
+
+local VERSION = require("hardcover_version")
+
+local RELEASE_API = "https://api.github.com/repos/billiam/hardcoverapp.koplugin/releases?per_page=1"
+
+local Github = {}
+
+function Github:newestRelease()
+  local responseBody = {}
+  local res, code, responseHeaders = https.request {
+    url = RELEASE_API,
+    sink = ltn12.sink.table(responseBody),
+  }
+
+  if code == 200 or code == 304 then
+    local data = json.decode(table.concat(responseBody), json.decode.simple)
+    if data and #data > 0 then
+      local tag = data[1].tag_name
+      local index = 1
+      for str in string.gmatch(tag, "([^.]+)") do
+        local part = tonumber(str)
+
+        if part < VERSION[index] then
+          return nil
+        elseif part > VERSION[index] then
+          return tag
+        end
+        index = index + 1
+      end
+    end
+  end
+end
+
+return Github
---
+++ lib/hardcover.lua
@@ -0,0 +1,178 @@
+-- wrapper around hardcover_api to add higher level methods
+local _ = require("gettext")
+local logger = require("logger")
+local util = require("util")
+
+local UIManager = require("ui/uimanager")
+
+local Notification = require("ui/widget/notification")
+
+local Api = require("lib/hardcover_api")
+local Book = require("lib/book")
+local User = require("lib/user")
+
+local SETTING = require("lib/constants/settings")
+
+local Hardcover = {}
+Hardcover.__index = Hardcover
+
+function Hardcover:new(o)
+  return setmetatable(o, self)
+end
+
+function Hardcover:updateCurrentBookStatus(status, privacy_setting_id)
+  self.cache:updateBookStatus(self.ui.document.file, status, privacy_setting_id)
+  if not self.state.book_status.id then
+    self.dialog_manager:showError("Book status could not be updated")
+  end
+end
+
+function Hardcover:changeBookVisibility(visibility)
+  self.cache:cacheUserBook()
+
+  if self.state.book_status.id then
+    self:updateCurrentBookStatus(self.state.book_status.status_id, visibility)
+  end
+end
+
+function Hardcover:linkBook(book)
+  local filename = self.ui.document.file
+
+  local delete = {}
+  local clear_keys = { "book_id", "edition_id", "edition_format", "pages", "title" }
+  for _, key in ipairs(clear_keys) do
+    if book[key] == nil then
+      table.insert(delete, key)
+    end
+  end
+
+  local new_settings = {
+    book_id = book.book_id,
+    edition_id = book.edition_id,
+    edition_format = book.edition_format,
+    pages = book.pages,
+    title = book.title,
+    _delete = delete
+  }
+
+  if self.state.book_status.book_id == new_settings.book_id and self.state.book_status.edition_id == new_settings.edition_id then
+    return
+  end
+
+  self.settings:updateBookSetting(filename, new_settings)
+  self.cache:cacheUserBook()
+
+  if book.book_id and self.state.book_status.id then
+    if new_settings.edition_id and new_settings.edition_id ~= self.state.book_status.edition_id then
+      -- update edition
+      self.state.book_status = Api:updateUserBook(
+        new_settings.book_id,
+        self.state.book_status.status_id,
+        self.state.book_status.privacy_setting_id,
+        new_settings.edition_id
+      ) or {}
+    end
+  end
+
+  return true
+end
+
+-- could be moved to book search model
+function Hardcover:findBookOptions(force_search)
+  local props = self.ui.document:getProps()
+  local identifiers = Book:parseIdentifiers(props.identifiers)
+  local user_id = User:getId()
+
+  if not force_search then
+    local book_lookup = Api:findBookByIdentifiers(identifiers, user_id)
+    if book_lookup then
+      return nil, { book_lookup }
+    end
+  end
+
+  local title = props.title
+  if not title or title == "" then
+    local _dir, path = util.splitFilePathName(self.ui.document.file)
+    local filename, _suffix = util.splitFileNameSuffix(path)
+
+    title = filename:gsub("_", " ")
+  end
+  return title, Api:findBooks(title, props.authors, user_id)
+end
+
+function Hardcover:autolinkBook(book)
+  if not book then
+    return
+  end
+
+  local linked = self:linkBook(book)
+  if linked then
+    UIManager:show(Notification:new {
+      text = _("Linked to: " .. book.title),
+    })
+  end
+end
+
+function Hardcover:linkBookByIsbn()
+  local props = self.ui.document:getProps()
+  local identifiers = Book:parseIdentifiers(props.identifiers)
+  if identifiers.isbn_10 or identifiers.isbn_13 then
+    local user_id = User:getId()
+    local book_lookup = Api:findBookByIdentifiers({
+        isbn_10 = identifiers.isbn_10,
+        isbn_13 = identifiers.isbn_13
+      },
+      user_id
+    )
+    if book_lookup then
+      self:autolinkBook(book_lookup)
+      return true
+    end
+  end
+end
+
+function Hardcover:linkBookByHardcover()
+  local props = self.ui.document:getProps()
+
+  local identifiers = Book:parseIdentifiers(props.identifiers)
+  if identifiers.book_slug or identifiers.edition_id then
+    local user_id = User:getId()
+    local book_lookup = Api:findBookByIdentifiers(
+      { book_slug = identifiers.book_slug, edition_id = identifiers.edition_id }, user_id)
+    if book_lookup then
+      self:autolinkBook(book_lookup)
+      return true
+    end
+  end
+end
+
+function Hardcover:linkBookByTitle()
+  local props = self.ui.document:getProps()
+
+  local results = Api:findBooks(props.title, props.authors, User:getId())
+  if results and #results > 0 then
+    self:autolinkBook(results[1])
+    return true
+  end
+end
+
+function Hardcover:tryAutolink()
+  if self.settings:bookLinked() then
+    return
+  end
+
+  local linked = false
+  if self.settings:readSetting(SETTING.LINK_BY_ISBN) then
+    linked = self:linkBookByIsbn()
+  end
+
+  if not linked and self.settings:readSetting(SETTING.LINK_BY_HARDCOVER) then
+    linked = self:linkBookByHardcover()
+  end
+
+  if not linked and self.settings:readSetting(SETTING.LINK_BY_TITLE) then
+    linked = self:linkBookByTitle()
+  end
+end
+
+return Hardcover
